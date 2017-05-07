ab0a71b5e2323b5efb62bc631b929cd206830ac0
Tobias Widén <tobias.widen@trioptima.com>
2017-05-07 14:00:40 +0200

    copy paste accelerometer code

--- src/main.cpp
+++ src/main.cpp
@@ -3,10 +3,9 @@ Front Sensor Array to locate an opponent robot or any other
 reflective object. Using the motors to turn, it scans its
 surroundings. If it senses an object, it turns on its yellow LED
 and attempts to face towards that object. */
-
+#include <avr/pgmspace.h>
 #include <Wire.h>
 #include <Zumo32U4.h>
-
 
 Zumo32U4LCD lcd;
 Zumo32U4Motors motors;
@@ -14,6 +13,70 @@ Zumo32U4ProximitySensors proxSensors;
 Zumo32U4ButtonA buttonA;
 Zumo32U4LineSensors lineSensors;
 Zumo32U4Buzzer buzzer;
+
+
+// Accelerometer Settings
+#define RA_SIZE 3  // number of readings to include in running average of accelerometer readings
+#define XY_ACCELERATION_THRESHOLD 2400  // for detection of contact (~16000 = magnitude of acceleration due to gravity)
+const char sound_effect[] PROGMEM = "O4 T100 V15 L4 MS g12>c12>e12>G6>E12 ML>G2"; // "charge" melody
+unsigned long contact_made_time;
+
+// RunningAverage class
+// based on RunningAverage library for Arduino
+// source:  http://playground.arduino.cc/Main/RunningAverage
+template <typename T>
+class RunningAverage
+{
+  public:
+    RunningAverage(void);
+    RunningAverage(int);
+    ~RunningAverage();
+    void clear();
+    void addValue(T);
+    T getAverage() const;
+    void fillValue(T, int);
+  protected:
+    int _size;
+    int _cnt;
+    int _idx;
+    T _sum;
+    T * _ar;
+    static T zero;
+};
+
+// Accelerometer Class -- extends the LSM303 class to support reading and averaging the x-y acceleration
+//   vectors from the onboard LSM303DLHC accelerometer/magnetometer
+class Accelerometer : public LSM303
+{
+  typedef struct acc_data_xy
+  {
+    unsigned long timestamp;
+    int x;
+    int y;
+    float dir;
+  } acc_data_xy;
+
+  public:
+    Accelerometer() : ra_x(RA_SIZE), ra_y(RA_SIZE) {};
+    ~Accelerometer() {};
+    void enable(void);
+    void getLogHeader(void);
+    void readAcceleration(unsigned long timestamp);
+    float len_xy() const;
+    float dir_xy() const;
+    int x_avg(void) const;
+    int y_avg(void) const;
+    long ss_xy_avg(void) const;
+    float dir_xy_avg(void) const;
+  private:
+    acc_data_xy last;
+    RunningAverage<int> ra_x;
+    RunningAverage<int> ra_y;
+};
+
+Accelerometer lsm303;
+boolean in_contact;  // set when accelerometer detects contact with opposing robot
+
 
 // A sensors reading must be greater than or equal to this
 // threshold in order for the program to consider that sensor as
@@ -82,8 +145,12 @@ uint16_t lastTimeObjectSeen = 0;
 void setup()
 {
   Serial.begin(115200);
+  Wire.begin();
+  lsm303.init();
+  lsm303.enable();
   proxSensors.initFrontSensor();
   lineSensors.initThreeSensors();
+  randomSeed((unsigned int) millis());
 
   // Wait for the user to press A before driving the motors.
   lcd.clear();
@@ -96,6 +163,7 @@ void setup()
     delay(1000);
   }
   buzzer.playNote(NOTE_G(4), 400, 15);
+  buzzer.playMode(PLAY_AUTOMATIC);
 }
 
 void turnRight()
@@ -224,5 +292,157 @@ void loop()
     {
       turnLeft();
     }
+  }
+}
+
+
+// class Accelerometer -- member function definitions
+
+// enable accelerometer only
+// to enable both accelerometer and magnetometer, call enableDefault() instead
+void Accelerometer::enable(void)
+{
+  // Enable Accelerometer
+  // 0x27 = 0b00100111
+  // Normal power mode, all axes enabled
+  writeAccReg(LSM303::CTRL_REG1_A, 0x27);
+
+  if (getDeviceType() == LSM303::device_DLHC)
+  writeAccReg(LSM303::CTRL_REG4_A, 0x08); // DLHC: enable high resolution mode
+}
+
+void Accelerometer::getLogHeader(void)
+{
+  Serial.print("millis    x      y     len     dir  | len_avg  dir_avg  |  avg_len");
+  Serial.println();
+}
+
+void Accelerometer::readAcceleration(unsigned long timestamp)
+{
+  readAcc();
+  if (a.x == last.x && a.y == last.y) return;
+
+  last.timestamp = timestamp;
+  last.x = a.x;
+  last.y = a.y;
+
+  ra_x.addValue(last.x);
+  ra_y.addValue(last.y);
+
+#ifdef LOG_SERIAL
+ Serial.print(last.timestamp);
+ Serial.print("  ");
+ Serial.print(last.x);
+ Serial.print("  ");
+ Serial.print(last.y);
+ Serial.print("  ");
+ Serial.print(len_xy());
+ Serial.print("  ");
+ Serial.print(dir_xy());
+ Serial.print("  |  ");
+ Serial.print(sqrt(static_cast<float>(ss_xy_avg())));
+ Serial.print("  ");
+ Serial.print(dir_xy_avg());
+ Serial.println();
+#endif
+}
+
+float Accelerometer::len_xy() const
+{
+  return sqrt(last.x*a.x + last.y*a.y);
+}
+
+float Accelerometer::dir_xy() const
+{
+  return atan2(last.x, last.y) * 180.0 / M_PI;
+}
+
+int Accelerometer::x_avg(void) const
+{
+  return ra_x.getAverage();
+}
+
+int Accelerometer::y_avg(void) const
+{
+  return ra_y.getAverage();
+}
+
+long Accelerometer::ss_xy_avg(void) const
+{
+  long x_avg_long = static_cast<long>(x_avg());
+  long y_avg_long = static_cast<long>(y_avg());
+  return x_avg_long*x_avg_long + y_avg_long*y_avg_long;
+}
+
+float Accelerometer::dir_xy_avg(void) const
+{
+  return atan2(static_cast<float>(x_avg()), static_cast<float>(y_avg())) * 180.0 / M_PI;
+}
+
+
+
+// RunningAverage class
+// based on RunningAverage library for Arduino
+// source:  http://playground.arduino.cc/Main/RunningAverage
+// author:  Rob.Tillart@gmail.com
+// Released to the public domain
+
+template <typename T>
+T RunningAverage<T>::zero = static_cast<T>(0);
+
+template <typename T>
+RunningAverage<T>::RunningAverage(int n)
+{
+  _size = n;
+  _ar = (T*) malloc(_size * sizeof(T));
+  clear();
+}
+
+template <typename T>
+RunningAverage<T>::~RunningAverage()
+{
+  free(_ar);
+}
+
+// resets all counters
+template <typename T>
+void RunningAverage<T>::clear()
+{
+  _cnt = 0;
+  _idx = 0;
+  _sum = zero;
+  for (int i = 0; i< _size; i++) _ar[i] = zero;  // needed to keep addValue simple
+}
+
+// adds a new value to the data-set
+template <typename T>
+void RunningAverage<T>::addValue(T f)
+{
+  _sum -= _ar[_idx];
+  _ar[_idx] = f;
+  _sum += _ar[_idx];
+  _idx++;
+  if (_idx == _size) _idx = 0;  // faster than %
+  if (_cnt < _size) _cnt++;
+}
+
+// returns the average of the data-set added so far
+template <typename T>
+T RunningAverage<T>::getAverage() const
+{
+  if (_cnt == 0) return zero; // NaN ?  math.h
+  return _sum / _cnt;
+}
+
+// fill the average with a value
+// the param number determines how often value is added (weight)
+// number should preferably be between 1 and size
+template <typename T>
+void RunningAverage<T>::fillValue(T value, int number)
+{
+  clear();
+  for (int i = 0; i < number; i++)
+  {
+    addValue(value);
   }
 }
